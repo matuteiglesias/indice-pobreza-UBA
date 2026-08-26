fe7fdfe22e25f54d806e0fe266cee594f1e5f192
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-26 05:36:23 -0300

    feat: expose poverty method contract

--- src/poverty_pipeline/science/__init__.py
+++ src/poverty_pipeline/science/__init__.py
@@ -15,10 +15,18 @@ from .household_poverty import (
     WeightUsePolicy,
     calculate_household_poverty,
 )
+from .method import (
+    AdultEquivalenceBand,
+    MethodContractError,
+    PovertyMethod,
+    load_poverty_method,
+    poverty_method_from_dict,
+)
 
 __all__ = [
-    "AdultEquivalenceCell", "ComparisonPolicy", "GapSignPolicy", "Household",
-    "HouseholdPoverty", "LinearIncome", "NormalizedPerson", "PovertyPolicies", "PovertyInputError",
-    "RegionalPeriodBasket", "ScientificDependencyContract", "WeightUsePolicy",
-    "calculate_household_poverty",
+    "AdultEquivalenceBand", "AdultEquivalenceCell", "ComparisonPolicy", "GapSignPolicy",
+    "Household", "HouseholdPoverty", "LinearIncome", "MethodContractError", "NormalizedPerson",
+    "PovertyMethod", "PovertyPolicies", "PovertyInputError", "RegionalPeriodBasket",
+    "ScientificDependencyContract", "WeightUsePolicy", "calculate_household_poverty",
+    "load_poverty_method", "poverty_method_from_dict",
 ]
